e02f15bbe54bfb53c0f1cd2e7617267702f89627
gfullsack <gfullsack@gmail.com>
2019-08-06 21:32:46 -0700

    SQL Code
    SQL Code

---
+++ Yelp_db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Merged_data (
+"business_id" INT ,
+"business_name" Varchar (50) ,
+"business_city"  Varchar (50) ,
+"inspection_score" INT ,
+"inspection_date" DATE ,
+"alias"  Varchar (50) ,
+"name"  Varchar (50) ,
+"categories"  Varchar (250) ,
+"coordinates"  Varchar (250) ,
+"price" Varchar (50) ,
+"rating" INT ,
+"review_count" INT 
